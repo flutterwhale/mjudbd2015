0a0eaee3ef21baf599f0e79089f67c151fbdcb72
flutterwhale <flutterwhale@hotmail.com>
2015-11-17 03:28:22 +0900

    package 설정
    기본폼 jsp
    login 기능추가 예정

--- groupwareSystem_team2/src/main/java/kr/ac/mju/prompt/controller/HomeController.java
+++ groupwareSystem_team2/src/main/java/kr/ac/mju/prompt/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.ac.mju;
+package kr.ac.mju.prompt.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
--- groupwareSystem_team2/src/main/webapp/WEB-INF/views/home.jsp
+++ groupwareSystem_team2/src/main/webapp/WEB-INF/views/home.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 <h1>
-	안현선
+	MASTER 
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
